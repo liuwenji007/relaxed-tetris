9d2488032b693e4c0c175f0e370371bf9e085165
liuwenji007 <liuwenjie007@qq.com>
2022-02-04 10:58:24 +0800

    initial commit

--- src/components/next/index.vue
+++ src/components/next/index.vue
@@ -1,10 +1,18 @@
+<!--
+ * @Author: lwj
+ * @Date: 2021-09-08 18:51:24
+ * @LastEditors: lwj
+ * @LastEditTime: 2022-02-04 10:57:16
+ * @Description: 
+ * @FilePath: /vue-tetris/src/components/next/index.vue
+-->
 <template>
   <div class="next">
     <div v-for="(item,index) in block">
       <b :class="e ? 'c' : ''" v-for="(e,k2) in item" />
     </div>
   </div>
-</template>
+</template>  
 
 <script src="./index.js">
 </script>
